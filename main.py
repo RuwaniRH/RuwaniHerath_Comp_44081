a2c9bf448ca53d13bdfca61b3b5485a096a566f7
RuwaniRH <155591311+RuwaniRH@users.noreply.github.com>
2025-06-12 22:22:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from data_loader import load_data
 from features import add_time_features, add_lag_features, add_rolling_features
 from model_redemption import ImprovedRedemptionModel
 from model_sales import SalesForecastModel
+from evaluate import evaluate_forecast, plot_forecast
 
 df = load_data('./data/Toronto Island Ferry Ticket Counts.csv')
 
@@ -21,3 +22,32 @@ redemption_forecast = redemption_model.run()
 print("Running Sales Forecast Model...")
 sales_model = SalesForecastModel(df, target='Sales Count')
 sales_forecast = sales_model.run()
+
+
+forecast_redemption = redemption_model.forecast(30)
+actual = df[['Redemption Count']].reset_index().rename(columns={'Timestamp': 'ds', 'Redemption Count': 'y'})
+merged = actual.merge(forecast_redemption[['ds', 'yhat', 'yhat_lower', 'yhat_upper']], on='ds')
+metrics = evaluate_forecast(
+    merged['y'],
+    merged['yhat'],
+    merged['yhat_lower'],
+    merged['yhat_upper']
+)
+plot_forecast(merged, merged, target_col='y', model_name='Redemption Model')
+print(metrics)
+
+
+forecast_sales = sales_model.forecast(30)
+actual = df[['Sales Count']].reset_index().rename(columns={'Timestamp': 'ds', 'Sales Count': 'y'})
+merged = actual.merge(forecast_sales[['ds', 'yhat', 'yhat_lower', 'yhat_upper']], on='ds')
+metrics = evaluate_forecast(
+  merged['y'], 
+  merged['yhat'], 
+  merged['yhat_lower'], 
+  merged['yhat_upper']
+)
+plot_forecast(merged, merged, target_col='y', model_name='Sales Model')
+print(metrics)
+
+
+
